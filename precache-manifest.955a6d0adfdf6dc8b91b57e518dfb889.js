fde5c1c04711203cb91139f7344820ad918baab5
SrMeuNome <vinicius10001999@gmail.com>
2020-05-30 16:10:02 -0300

    Updates

---
+++ precache-manifest.955a6d0adfdf6dc8b91b57e518dfb889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "302cf5144e0afdbb101817a4a3050433",
+    "url": "/TicTacToeReact/index.html"
+  },
+  {
+    "revision": "bcb306e7b87eba540510",
+    "url": "/TicTacToeReact/static/css/main.4e93fd34.chunk.css"
+  },
+  {
+    "revision": "788bf36ecea4c4a333c0",
+    "url": "/TicTacToeReact/static/js/2.dc270b52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToeReact/static/js/2.dc270b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb306e7b87eba540510",
+    "url": "/TicTacToeReact/static/js/main.dba836a2.chunk.js"
+  },
+  {
+    "revision": "0170f511bc2ba25dba37",
+    "url": "/TicTacToeReact/static/js/runtime-main.aa5e114d.js"
+  }
+]);
